c1341b4fa8cf545ea7baa4f580de4e274ad079eb
Dyslaw Pesail <dyslaw.pesail@hotmail.com>
2014-11-04 09:28:56 +0000

    Merging with master, I really don't need other branches at this stage

--- src/constr/syntax/regular_expression_parser.h
+++ src/constr/syntax/regular_expression_parser.h
@@ -5,9 +5,59 @@
 
 namespace constr {
     namespace syntax {
-    
-        typedef 
-    
+        typedef util::fsm::input_traits<char> re_lex_input_traits;
+        class re_token {
+        public:
+            enum value {
+               TerminalUpperLetter,
+               TerminalLowerLetter,
+               TerminalDigit,
+               TerminalBackslash
+               TerminalLiteral
+               TerminalLeftParenthesis,
+               TerminalRightParenthesis,
+               TerminalLeftSquareBracket,
+               TerminalRightSquareBracket,
+               TerminalLeftCurlyBracket,
+               TerminalRightCurlyBracket,
+               TerminalAlternative,
+               TerminalStart,
+               TerminalPlus,
+               TerminalRange,
+               TerminalEndOfStream
+            } symbol;
+            re_token(value Symbol, char Text = '\0') : symbol(Symbol), text(Text) {}
+            char text;
+            std::list<std::shared_ptr<re_token>> children;
+        };
+        class re_lex_output_traits {
+        public:
+            typedef std::shared_ptr<re_token> output_type;
+            typedef std::list<output_type> return_type
+        };
+        class re_lex_state_traits {
+        public:
+            enum value {
+                Start = 0,
+                Error,
+                Final
+            }; 
+            typedef value state_type;
+            static const value start_state, error_state, final_state;
+        };
+        const re_lex_state_traits::value re_lex_state_traits::start_state = re_lex_state_traits::Start;
+        const re_lex_state_traits::value re_lex_state_traits::start_state = re_lex_state_traits::Error;
+        const re_lex_state_traits::value re_lex_state_traits::start_state = re_lex_state_traits::Final;
+        class re_lex_transition_traits : public util::fsm::transition_traits<re_lex_input_traits, r_lex_state_traits, std::list<re_lex_input_traits::input_typee>> {
+        public:
+            static void initialize(std::map<re_lex_state_traits::state_type, transition_type>& States) {
+                typedef re_lex_state_traits::state_type state;
+                typedef std::list<re_lex_input_traits::input_type>::iterator iterator;
+
+            }
+        };
+
+        typedef util::fsm::automaton<re_transition_traits, re_strategy_traits> regular_expression_parser;
     }
 }
 
